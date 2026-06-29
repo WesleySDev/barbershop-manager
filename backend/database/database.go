8f9a460383e41223dabf2ba64641844186ade090
WesleySDev <you@example.com>
2026-06-28 22:36:37 -0300

    Adiciona função de conexão com o banco de dados PostgreSQL

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=postgres password=gaza dbname=barbershop port=5432 sslmode=disable "
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erro ao conectar ao banco:", err)
+	} else {
+		log.Println("Conectado ao PostgreSQL com sucesso!")
+	}
+}
