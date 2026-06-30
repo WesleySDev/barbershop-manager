8972466a27e531ffa0c75b794ab85bb7413d8bc1
WesleySDev <you@example.com>
2026-06-29 22:17:54 -0300

    Refactor conexão com o banco de dados para melhorar a legibilidade e corrigir a inicialização da variável de erro

--- backend/database/database.go
+++ backend/database/database.go
@@ -11,12 +11,17 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=gaza dbname=barbershop port=5432 sslmode=disable "
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=gaza dbname=barbershop port=5432 sslmode=disable"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Erro ao conectar ao banco:", err)
-	} else {
-		log.Println("Conectado ao PostgreSQL com sucesso!")
 	}
-	DB.AutoMigrate(&models.Client{})
+	log.Println("Conectado ao PostgreSQL com sucesso!")
+
+	if err := DB.AutoMigrate(&models.Client{}); err != nil {
+		log.Fatal("Erro ao criar tabela:", err)
+	}
+	log.Println("Tabela clients sincronizada com sucesso!")
+
 }
